e31b38e546fe987b443328ddae688086cb7aef76
Przemek Jeske <jeskenis@gmail.com>
2025-12-04 15:37:17 +0100

    parent html node added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,70 @@ class HTMLNode:
             for key in self.props:
                 assembled_props += f' {key}="{self.props[key]}"'
         return assembled_props
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None,
+        value: str,
+        props: dict | None = None,
+    ) -> None:
+        super().__init__(tag, value, props=props)
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str,
+        children: list,
+        props: dict | None = None,
+    ) -> None:
+        super().__init__(tag, value=None, children=children, props=props)
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.props}, {self.children})"
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("Invalid - missing tag")
+        if not self.children:
+            raise ValueError("Invalid - ParentNode is missing children,")
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+
+def main():
+    node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    print(node.to_html())
+
+    node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+
+    print(node.to_html())
+
+
+if __name__ == "__main__":
+    main()
